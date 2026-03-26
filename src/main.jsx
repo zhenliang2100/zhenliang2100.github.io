f0c1f470f1b1d30c94ce8731724229ca1bd6cd55
zhenliang2100 <zhenliang2100@gmail.com>
2026-03-26 15:16:25 +0800

    配置 Hash 路由支持 GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/i" element={<App />} />
+      </Routes>
+    </HashRouter>
   </StrictMode>,
 )
